40d2e75750c8a16369aac84831a9087b06c31baa
David Ocsko <docsko@n-ware.eu>
2026-06-07 22:39:01 +0200

    IOS and new apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.memoryatlas.app',
+  appId: 'com.memoryatlas.com',
   appName: 'Memory Atlas',
   webDir: 'dist'
 };
